a82ae92fda31ab752e21a9bfd156d86ff7ad53a0
tracer-nju <tracer@nju.edu.cn>
2022-03-07 16:55:59 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  16:55:59 up  2:41,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 #include <assert.h>
 
+static struct option longopts[] = {
+	{ "show-pids",		0, NULL, 'p' },
+	{ "numeric-sort",	0, NULL, 'n' },
+	{ "version",		0, NULL, 'V' },
+	{ 0,				0, 0,	 0   }
+};
+
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+	char opt;
+	int option_index;
+
+	while ((opt = getopt_long(argc, argv, "pnV", longopts, &option_index)) != -1) {
+		switch (opt) {
+		case 'p':
+			break;
+		case 'n':
+			break;
+		case 'V':
+			break;
+		default:
+			fprintf(stderr, "Error\n");
+			exit(1);
+		}
+		printf("%c\n", opt);
+	}
+
+	return 0;
 }
