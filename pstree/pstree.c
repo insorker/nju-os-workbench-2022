a931cb2e3214235481c0b80504e6b25e64bcfeac
tracer-nju <tracer@nju.edu.cn>
2022-03-07 17:19:26 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  17:19:26 up  3:05,  0 users,  load average: 0.52, 0.58, 0.59

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <dirent.h>
 #include <assert.h>
+
+#define VERSION "Version: 0.0.1\n"
+
+struct process_state {
+	int pid;
+	int ppid;
+	char *name;
+};
 
 static struct option longopts[] = {
 	{ "show-pids",		0, NULL, 'p' },
@@ -11,9 +20,16 @@ static struct option longopts[] = {
 	{ 0,				0, 0,	 0   }
 };
 
+void getProcessState();
+void printVersion();
+
 int main(int argc, char *argv[]) {
 	char opt;
 	int option_index;
+
+	if (argc == 1) {
+		getProcessState();
+	}
 
 	while ((opt = getopt_long(argc, argv, "pnV", longopts, &option_index)) != -1) {
 		switch (opt) {
@@ -29,7 +45,23 @@ int main(int argc, char *argv[]) {
 		}
 		printf("%c\n", opt);
 	}
-	printf("%d\n", opt); 
 
 	return 0;
+}
+
+void getProcessState() {
+	DIR *d;
+	struct dirent *dir;
+
+	d = opendir("/proc");
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			printf("%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
+}
+
+void printVersion() {
+	fprintf(stdout, VERSION);
 }
