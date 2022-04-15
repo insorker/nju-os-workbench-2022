75019cb081e53d0d7e9b0c4aa9845381e613e837
tracer-nju <tracer@nju.edu.cn>
2022-04-15 17:41:15 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  17:41:15 up  7:04,  0 users,  load average: 0.52, 0.58, 0.59

--- kernel/test/test.c
+++ kernel/test/test.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <thread.h>
 
-static void entry(int tid) { pmm->alloc(128); }
+static void entry(int tid) { }
 static void finish() { printf("End\n"); }
 
 int main() {
 	pmm->init();
+	entry(1);
+	finish();
 }
