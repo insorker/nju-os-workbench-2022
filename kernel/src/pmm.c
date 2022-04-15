7dffa906b1705783e358d8c5170e98baa1a1bc33
tracer-nju <tracer@nju.edu.cn>
2022-04-15 17:25:42 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  17:25:42 up  6:48,  0 users,  load average: 0.52, 0.58, 0.59

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -7,10 +7,18 @@ static void *kalloc(size_t size) {
 static void kfree(void *ptr) {
 }
 
+#ifdef TEST
+static void pmm_init() {
+	char* pmsize = malloc(HEAP_SIZE);
+	heap.start = ptr;
+	heap.end   = ptr + HEAP_SIZE;
+	printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+#else
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
+#endif
 
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
