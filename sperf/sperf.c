74648d7e6f4573896e74fd216192b686ad06e1c8
tracer-nju <tracer@nju.edu.cn>
2022-04-19 21:45:12 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  21:45:12 up  9:36,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -16,10 +16,12 @@ int main(int argc, char *argv[], char *envp[]) {
 	
 	pid = fork();
 	if (pid == 0) {
-		close(pipefd[1]);
-		dup(1);
 		close(1);
-		close(pipefd[0]);
+		close(0);
+		/* close(pipefd[1]); */
+		/* dup(1); */
+		/* close(1); */
+		/* close(pipefd[0]); */
 
 		execve("/bin/strace", strace_argv, envp);
 		zassert(0, "execve failed");
