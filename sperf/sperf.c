f0b260b6c7efaa0cab388e9bf5c224fde3cf4f5e
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:38:19 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:38:19 up  8:29,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+#define zassert(x, s) \
+	do { if (x == 0) { printf("%s\n", s); assert(x); } } while (0)
 
 int main(int argc, char *argv[], char *envp[]) {
-	if (argc < 2) exit(1);
+	zassert(argc >= 2, "need at least one argument");
+
 	char *strace_argv[] = { "strace", argv[1], NULL };
-	execve("/bin/strace", strace_argv, envp);
+	int pipefd[2];
+	int pid;
+
+	zassert(pipe(pipefd) == 0, "create pipe failed");
+	
+	pid = fork();
+	if (pid == 0) {
+		printf("%d ", pipefd[1]);
+		close(0);
+		dup(pipefd[1]);
+
+		execve("/bin/strace", strace_argv, envp);
+		zassert(0, "execve failed");
+	}
+	else {
+	}
 }
