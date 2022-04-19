b179429677970973fcffc838aed0b8a0a9f610b8
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:06:20 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:06:20 up  7:57,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,8 +3,11 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-	char *exec_argv[] = { "yes", NULL };
-	execve("yes", exec_argv, NULL);
+	char *exec_argv[] = { "strace", "ls", NULL, };
+	char *exec_envp[] = { "PATH=/bin", NULL, };
+	execve("strace",          exec_argv, exec_envp);
+	execve("/bin/strace",     exec_argv, exec_envp);
+	execve("/usr/bin/strace", exec_argv, exec_envp);
 	perror(argv[0]);
 	exit(EXIT_FAILURE);
 }
