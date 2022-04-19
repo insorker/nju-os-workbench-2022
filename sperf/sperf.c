acb545de87798c222ebfc1fe00da6e058edf59a2
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:17:34 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:17:34 up  8:08,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,6 +5,9 @@
 int main(int argc, char *argv[], char *envp[]) {
 	if (argc < 3) exit(1);
 	/* execve(argv[1], argv + 1, envp); */
+	if ((argv + 1)[2] == NULL) {
+		printf("%d\n", 123);
+	}
 	printf("%s", (argv + 1)[0]);
 	/* execve("strace", argv + 1, envp); */
 }
