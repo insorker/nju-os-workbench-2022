5ece6b30dad331b6435957f9ae875792f879f205
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:13:59 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:13:59 up  8:04,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,15 +3,6 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-	for (int i = 0; argv[i] != NULL; i ++ )
-		printf("%d ", i);
-	
-	/* char *exec_argv[] = { "strace", "ls", NULL, }; */
-	/* [> char *exec_envp[] = { "PATH=/bin", NULL, }; <] */
-	/* char *exec_envp[] = { "", NULL, }; */
-	/* [> execve("strace",          exec_argv, exec_envp); <] */
-	/* execve("/bin/strace",     exec_argv, envp); */
-	/* [> execve("/usr/bin/strace", exec_argv, exec_envp); <] */
-	/* perror(argv[0]); */
-	exit(EXIT_FAILURE);
+	if (argc <= 3) exit(1);
+	execve(argv[1], argv + 1, envp);
 }
