73edd2fc22e14b6232c49f837f3398bf15b1a2cb
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:16:24 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:16:24 up  8:07,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-	if (argc <= 3) exit(1);
+	if (argc < 3) exit(1);
 	/* execve(argv[1], argv + 1, envp); */
 	printf("%s", *(argv + 1));
 	/* execve("strace", argv + 1, envp); */
