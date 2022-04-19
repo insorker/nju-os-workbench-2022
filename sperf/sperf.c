c241d80df0c6d3e5978681fd71b508452293e5e4
tracer-nju <tracer@nju.edu.cn>
2022-04-19 20:11:13 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:11:13 up  8:02,  0 users,  load average: 0.52, 0.58, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,12 +3,15 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]) {
-	char *exec_argv[] = { "strace", "ls", NULL, };
-	/* char *exec_envp[] = { "PATH=/bin", NULL, }; */
-	char *exec_envp[] = { "", NULL, };
-	/* execve("strace",          exec_argv, exec_envp); */
-	execve("/bin/strace",     exec_argv, envp);
-	/* execve("/usr/bin/strace", exec_argv, exec_envp); */
-	perror(argv[0]);
+	for (int i = 0; argv[i] != NULL; i ++ )
+		printf("%d ", i);
+	
+	/* char *exec_argv[] = { "strace", "ls", NULL, }; */
+	/* [> char *exec_envp[] = { "PATH=/bin", NULL, }; <] */
+	/* char *exec_envp[] = { "", NULL, }; */
+	/* [> execve("strace",          exec_argv, exec_envp); <] */
+	/* execve("/bin/strace",     exec_argv, envp); */
+	/* [> execve("/usr/bin/strace", exec_argv, exec_envp); <] */
+	/* perror(argv[0]); */
 	exit(EXIT_FAILURE);
 }
