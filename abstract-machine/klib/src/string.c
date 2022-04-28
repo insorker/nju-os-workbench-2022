fbef7814d11e8eab80ebd2f0963ede387fb4c12c
insorker <2677331029@qq.com>
2022-04-28 14:55:25 +0800

    add memset

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -29,7 +29,14 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	if (s) {
+		char *ps = (char *)s;
+		while (n--) {
+			*ps++ = c;
+		}
+	}
+
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
