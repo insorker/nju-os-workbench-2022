bb83ffca5b471d7779d9a2ef81bd57a24d1f9cf1
tracer-nju <tracer@nju.edu.cn>
2022-05-02 20:34:29 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  20:34:29 up 10:02,  0 users,  load average: 0.52, 0.58, 0.59

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -8,12 +8,12 @@ static int wrapper_num;
 static char *wrapper_func[] = {
 	"int ", "__expr_wrapper_", "(){return ", ";}\n",
 };
-static char wrapper_file[] = "crepl_tmp";
+static char wrapper_file[] = "/tmp/crepl_tmp";
 static FILE *wrapper_fd;
 static char *const gcc_argv[] = {
 	"-fPIC",
 	"-shared",
-	"crepl_tmp",
+	wrapper_file,
 	"-o crepl_tmp.so"
 };
 
@@ -50,12 +50,12 @@ int main(int argc, char *argv[]) {
 					wrapper_func[0], wrapper_func[1], wrapper_num, wrapper_func[2], line, wrapper_func[3]);
 			fclose(wrapper_fd);
 
-			compile();
-			handle = dlopen("./crepl_tmp.so", RTLD_LAZY);
-			sscanf(wrapper_buf, "%s%d", wrapper_func[1], &wrapper_num);
-			wrapper = (int (*)()) dlsym(handle, wrapper_buf);
-			wrapper();
-			wrapper_num++;
+			/* compile(); */
+			/* handle = dlopen("./crepl_tmp.so", RTLD_LAZY); */
+			/* sscanf(wrapper_buf, "%s%d", wrapper_func[1], &wrapper_num); */
+			/* wrapper = (int (*)()) dlsym(handle, wrapper_buf); */
+			/* wrapper(); */
+			/* wrapper_num++; */
 		}
   }
 }
