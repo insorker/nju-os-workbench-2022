976cc8f76b4ab343f4d18a9c72da2ae1c1431d9d
tracer-nju <tracer@nju.edu.cn>
2022-05-02 16:45:09 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  16:45:09 up  6:13,  0 users,  load average: 0.52, 0.58, 0.59

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
+static int wrapper_num;
+static char *wrapper[] = {
+	"int __expr_wrapper_", "(){return ", ";}\n",
+};
+static char *file_path = "/tmp/crepl_tmp";
+
+/* #define wrapper_assemble() \ */
+/*   (wrapper[0] #wrapper_num) */
+#define wrapper_assemble(a, b) \
+	b
+
 int main(int argc, char *argv[]) {
+	FILE *fp = fopen(file_path, "w");
+	fclose(fp);
   static char line[4096];
+	
   while (1) {
     printf("crepl> ");
     fflush(stdout);
@@ -12,7 +26,13 @@ int main(int argc, char *argv[]) {
     }
 		int len = strlen(line);
 		if (len >= 3 && !strncmp(line, "int", 3)) {
-			printf("%s\n", line);
+			// function define
+		}
+		else {
+			// expression
+			fp = fopen(file_path, "a");
+			fprintf(fp, "%s%d%s%s%s", wrapper[0], wrapper_num, wrapper[1], line, wrapper[2]);
+			wrapper_num++;
 		}
 
     printf("Got %zu chars.\n", strlen(line)); // ??
