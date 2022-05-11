bb2569915dbdffc7b0737e98504bad592a9ca60a
tracer-nju <tracer@nju.edu.cn>
2022-05-11 16:01:49 +0800

    Linux LAPTOP-INSORKER 4.4.0-19041-Microsoft #1237-Microsoft Sat Sep 11 14:32:00 PST 2021 x86_64 x86_64 x86_64 GNU/Linux  16:01:49 up 40 min,  0 users,  load average: 0.52, 0.58, 0.59

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,26 +1,10 @@
 #include <kernel.h>
 #include <klib.h>
 
-void alloc(int sz) {
-	void *a = pmm->alloc(sz);
-	pmm->free(a);
-}
-
-void pmm_test() {
-	alloc(1);
-	alloc(5);
-	alloc(10);
-	alloc(32);
-	alloc(4096);
-	alloc(4096);
-	alloc(4096);
-}
-
 int main() {
   ioe_init();
 	os->init();
-	pmm_test();
 	/* cte_init(os->trap); */
-  /* mpe_init(os->run); */
+	mpe_init(os->run);
   return 1;
 }
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,11 +1,30 @@
 #include <common.h>
 
+#define TEST_PMM 1
+#if TEST_PMM
+#define alloc(sz) pmm->alloc(sz)
+#define free(addr) pmm->free(addr)
+void alloc_and_free() {
+	void *a;
+	a = alloc(1);					free(a);
+	a = alloc(128);				free(a);
+	a = alloc(4096);			free(a);
+	a = alloc(1 << 16);		free(a);
+	a = alloc(1 << 17);		free(a);
+}
+void pmm_test() {
+	alloc_and_free();
+}
+#endif
+
 static void os_init() {
   pmm->init();
 }
 
 static void os_run() {
-	pmm->alloc(1);
+#if TEST_PMM
+	pmm_test();
+#endif
 /*   for (const char *s = "Hello World from CPU #*\n"; *s; s++) { */
 /* #ifndef TEST */
 /*     putch(*s == '*' ? '0' + cpu_current() : *s); */
